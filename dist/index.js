9919bd31e3bdde20932906fb767e3fdf2b29c172
Saul Hamadani <saulhamadani@gmail.com>
2021-11-02 19:13:29 -0400

    stuff

--- dist/index.js
+++ dist/index.js
@@ -12877,7 +12877,7 @@ function updateDescriptionsMaybe(stories, releaseUrl, shouldUpdateDescription) {
 function addEndStateId(story, workflows, endStateName) {
     console.log("hello from index")
     console.log(`Could not locate workflow: ${workflows.map(workflow => console.log(workflow.id))}`)
-    console.log(`Could not locate story: ${story.workflowId}`)
+    console.log(`Could not locate story: ${story.workflow_id}`)
 
     const workflow = workflows.find(
         workflow => workflow.id === story.workflow_id
